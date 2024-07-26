86290102e4d66174c76f9991fa023f259d33618c
Peter Mullins <jc310xc@gmail.com>
2024-07-25 23:44:49 -0400

    strip out totem controller proj

--- totemController/src/totemHeaders.h
+++
@@ -1,6 +0,0 @@
-#include <ble/bleManager.h>
-#include <screen/colorInterpolator.h>
-#include <screen/colorProvider.h>
-#include <screen/solidColorProvider.h>
-#include <screen/randomColorProvider.h>
-#include <screen/twoColorSwapProvider.h>
